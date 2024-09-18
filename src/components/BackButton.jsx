ce84e431a08507d5b4e6c1ea45d37aff92c8fb85
sahildolia <sahildolia.456@gmail.com>
2024-09-18 18:46:23 +0530

    Code updated

---
+++ src/components/BackButton.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Button from './Button';
+import { useNavigate } from 'react-router-dom';
+export default function BackButton() {
+const navigate = useNavigate()
+
+  return (
+    <Button type="back" onClick={(e) => {
+        e.preventDefault();
+        navigate(-1)
+      }}>Back</Button>  )
+}
